c00fdef3ec341ee7efed7a3d48db8db7b75697ee
Jon Maddox <jon@jonmaddox.com>
2013-10-29 16:20:05 -0400

    drop the caching

--- lib/giant_bomb.rb
+++ lib/giant_bomb.rb
@@ -1,7 +1,6 @@
 require 'httparty'
 
 directory = File.expand_path(File.dirname(__FILE__))
-require File.join(directory, 'giant_bomb', 'httparty_icebox')
 require File.join(directory, 'giant_bomb', 'core_extensions')
 require File.join(directory, 'giant_bomb', 'attributes')
 require File.join(directory, 'giant_bomb', 'search')
